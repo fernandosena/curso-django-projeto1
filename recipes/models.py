05ab047aee726fc7ebb3d27db2273b2b74f1be27
Fernando Carvalho de Sena <fernandocarvalho.sena@gmail.com>
2023-06-04 19:59:24 -0300

    Aula renomenado um projeto django

--- recipes/models.py
+++ recipes/models.py
@@ -18,14 +18,18 @@ class Recipe(models.Model):
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
-    preparation_steps_id_html = models.BooleanField(default=False)
+    preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/cover/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True, default=None
+    )
 
     def __str__(self):
         return self.title
